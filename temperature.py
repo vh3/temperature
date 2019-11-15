3d5ef82852b0b218ae14bcc34873bad676fc7452
Vince Hartung <vincent.hartung@gmail.com>
2019-11-14 20:46:40 -0500

    added email capability to send results

--- temperature.py
+++ temperature.py
@@ -16,7 +16,7 @@ import csv
 delay_between_readings = 1.0
 
 # set the max length of time we will record data, in seconds
-max_time = 10
+max_time = 5.0
 
 # Find the filename associated with the for the first temperature sensor device
 path = '/sys/bus/w1/devices/'
@@ -135,3 +135,62 @@ while (elapsed_time <= max_time):
 with open(output_filename, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerows(reading_data)
+
+# Send this file to Sophie!
+def send_email(filename,timestamp):
+
+    import email, smtplib, ssl
+
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    subject = "Temperature sensor experiment " + timestamp
+    body = "Temperature sensor experiment data sent to you by Sophie's data robot."
+    sender_email = "data.robot42@gmail.com"
+    receiver_email = "vincent.hartung@gmail.com"
+    password = ""
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+
+    # filename = "document.pdf"  # In same directory as script
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+
+    return
+
+print (f'Attempting to email the output file ({output_filename}) to Sophie.')
+send_email(output_filename,timestamp)
+print ('Done.')

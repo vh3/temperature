74274bbbc74cf13b8f4f433d9a8ea9fdb8dba555
vh3 <34223414+vh3@users.noreply.github.com>
2019-11-17 09:10:15 -0500

    Added a check for missing sensor data.

--- temperature.py
+++ temperature.py
@@ -1,15 +1,15 @@
 # temperature.py
-# A script intended to read temperature information from a set of DS18B20 temperature sensors
-# and send to a comma-delimited file
+# A script intended to read temperature information from a set of DS18B20 temperature sensors connected to a Raspberry Pi and send to a comma-delimited (.csv) file.
 
 # 08 Nov 2019, vh3, starter from Adafruit lesson here: https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/overview
-# 10 Nov 2019, vh3, added functions for multiple sensors, formatted output for graphing
+# 10 Nov 2019, vh3, added functions for multiple sensors, formatted output for graphing in Excel
 # 14 Nov 2019, vh3, added email support after: https://realpython.com/python-send-email/
-# 17 Nov 2019, vh3, moved email function to a seperate file
+# 17 Nov 2019, vh3, moved email function to its own file
 
-# setup - this script was developed on a Raspberry PI 3+ running Raspian Linux.
-# setup - sensor connection for the digital DS18B20 temperature sensors is super easy.
-# setup - sensors were connected according to adafruit instructions: https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/overview
+# setup - This script was developed on a Raspberry PI 3+ running Raspian Linux.
+# setup - Sensor connection for the digital DS18B20 temperature sensors is super easy and reading the output from these digital sensors is as easy as parsing data out of a text file.
+# setup - Sensors were connected according to Adafruit tutoria: https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/overview
+# setup - All the sensors were connected to the same pins.  Each sensor has a unique identiifer that the operating system recognizes and writes to a unique output file.
 
 import glob
 import time
@@ -18,10 +18,10 @@ import os.path
 import csv
 
 # Optional: Do we want to email the output file somewhere?
-# If you want to use the email functionality, you will need to set up and email account for your robot.
-# Instructions for this are available here: https://realpython.com/python-send-email/
+# If you want to use the email functionality, you will need to set up and email account for your robot and edit the robot_send.py with sender details
+# Instructions for email account setup are available here: https://realpython.com/python-send-email/
 email_result = 0
-email_destination = "adestination@adomain.com"
+email_destination = "<destination email address here>"
 
 # Set the number of seconds to pause after each reading.
 # Note that we don't control the total amount of time it takes
@@ -31,27 +31,35 @@ delay_between_readings = 1.0
 # set the max length of time we will record data, in seconds
 max_time = 5.0
 
-# For certain long-running data collection activities, we may want to write the data
+# For certain long-running data collection activities (overnight, for example), we may want to write the data
 # one reading at a time in case there is some power failure or other stoppage in data collection before it is complete.
 write_immediate = 0
 if delay_between_readings >= 30.0 and max_time >= 600.0:
     write_immediate = 1
 
-# Find the output filenames associated with the temperature sensor devices.
-# The temperature sensors are "Dallas 1-wire protocol" devices, whose outputs are found here:
+# The temperature sensors are "Dallas 1-wire protocol" devices, whose outputs are found here in this root path:
 path = '/sys/bus/w1/devices/'
 
-# get the names of all the DS18B20 devices (they start with the prefix "28")
-# count the number of devices
+# Find all the output filenames associated with the temperature sensor devices.
+# Create a list of paths we can iterate over when we want to read all the individual sensors later 
+# The names of all the DS18B20 devices start with the prefix "28"
+pathlist = glob.glob(path + "28*")
 
+# Check that there are is sensor data available at all.  There is no point continuing if there are not.
+if len(pathlist) == 0:
+    print ('No DS18B20 devices were found.  It could be they are not connected, or 1-wire protocol is not enabled in raspi-config.')
+    print ('Done.')
+    exit()
+
+# Initialize a counter for the number of devices
 device_counter = 0
 # Initialize empty list to hold the path names for each device
 device_paths = []
 # Initialize an empty list to hold the unique identifier for each device
 device_ids = []
 
-# Create a list of paths we can iterate over when we want to read all the individual sensors later 
-for dev in glob.glob(path + '28*'):
+# Iterate over all the paths 
+for dev in pathlist:
 
     identifier = os.path.split(dev)[1]
     device_counter += 1
@@ -60,11 +68,7 @@ for dev in glob.glob(path + '28*'):
     device_paths.append(device_file)
     device_ids.append(identifier)
 
-# Settings for the first device
-# device_folder = glob.glob(path + '28*')[0]
-# device_file = device_folder + '/w1_slave'
-
-# initialize the time.  ToDo: figure out the types and use only a single reference variable to format the display
+# initialize the experiment starting time.  ToDo: figure out the types and use only a single reference variable to format the display (we mixed a few online examples here)
 start_time = time.time()
 start_display = time.asctime()
 print (f'Experiment started at: {start_display}')
@@ -105,8 +109,9 @@ def read_temp_raw(filename):
     return lines
 
 # Read all the sensors and return their values
-def read_temp2(sensor_filelist):
+def read_temp(sensor_filelist):
 
+    # Initialize an empty list to hold the temerature readings from all the sensors
     result_list = []
 
     # Iterate over each of the sensor filenames provided to this function, get a reading and append to result_list
@@ -127,10 +132,7 @@ def read_temp2(sensor_filelist):
             # temp_f = temp_c * 9.0 / 5.0 + 32.0
             result_list.append(temp_c)           
 
-    return result_list        
-
-# test out put new read_temp2function
-# print (f'output={read_temp2(device_paths)}')
+    return result_list
 
 # Start a timer
 elapsed_time = 0
@@ -141,7 +143,7 @@ reading_data = []
 # Loop, reading temperature and writing to file until the max time is reached
 while (elapsed_time <= max_time):
     elapsed_time = time.time() - start_time
-    data = read_temp2(device_paths)
+    data = read_temp(device_paths)
     data.insert(0,round(elapsed_time,0))
     print(data)
     reading_data.append(data)
@@ -167,4 +169,5 @@ if email_result == 1:
     from robot_send import send_email
     print (f'Attempting to email the output file ({output_filename}) to {email_destination}.')
     send_email(email_destination, output_filename, timestamp)
-    print ('Done.')
+
+print ('Done.')
